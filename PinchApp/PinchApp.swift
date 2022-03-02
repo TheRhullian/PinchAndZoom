399c0563b01239fa9ac3654fff69e402d4828070
Rhullian Damião <rhullian4545@gmail.com>
2022-03-02 11:48:55 -0300

    Feature: Project Init

---
+++ PinchApp/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Rhullian Damião on 02/03/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
