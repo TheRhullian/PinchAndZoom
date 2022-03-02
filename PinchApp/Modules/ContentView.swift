d58015101f8f01eca1419a2f18d1a070cebf11c8
Rhullian Damião <rhullian4545@gmail.com>
2022-03-02 12:28:59 -0300

    Feature: Drag Gesture added

--- PinchApp/Modules/ContentView.swift
+++ PinchApp/Modules/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     var body: some View {
         NavigationView{
@@ -17,6 +18,7 @@ struct ContentView: View {
                 Image.frontCoverPage
                     .pageConfiguration()
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                     .onTapGesture(count: 2) {
                         switch imageScale {
@@ -26,10 +28,23 @@ struct ContentView: View {
                             }
                         default:
                             withAnimation(.spring()) {
-                                imageScale = 1
+                                resetImageState()
                             }
                         }
                     }
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
             }//: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
@@ -38,6 +53,14 @@ struct ContentView: View {
             }
         }//: NAVIGATION VIEW
         .navigationViewStyle(.stack)
+    }
+    
+    // Function
+    func resetImageState() {
+        withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
     }
 }
 
